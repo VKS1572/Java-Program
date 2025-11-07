fd04d02cab3d7949d8777225f0e5af04a154c694
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2025-11-07 19:32:12 +0530

    Add files via upload

---
+++ D4P5.java
@@ -0,0 +1,22 @@
+// WriteaJavaprogramthattakesayearfromtheuserandprintwhetherthat
+//year is a leap year or not.
+
+import java.util.*;
+
+public class D4P5 {
+
+    public static void main(String[] args) {
+       try( Scanner sc = new Scanner(System.in)){
+        System.out.print("Input the year: ");
+        int year = sc.nextInt();
+       
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+        if (x && (y || z)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}}

a3d4b4644eb0f0a0bbf996c3dbcf87fb28e4235d
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2025-12-12 21:20:44 +0530

    Add files via upload

---
+++ D4P2.java
@@ -0,0 +1,15 @@
+/* FinishthefollowingcodesothatitprintsYouhaveafeverifyourtemperature
+ is above 100 and otherwise prints You don't have a fever
+
+*/
+public class D4P2 {
+    public static void main(String[] args) {
+        double temp = 103.5;
+    if (temp > 100){
+System.out.println("You have fever");
+    }
+else{
+    System.out.println("You don't have fever");
+}
+}
+}

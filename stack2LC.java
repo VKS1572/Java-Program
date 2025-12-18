1292394f4773d91fc27b4d8849d7662123ceac6b
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2025-12-18 19:22:39 +0530

    Implement removeDuplicates method in Solution class
    Added a method to remove duplicates from a string based on frequency.

--- stack2LC.java
+++ stack2LC.java
@@ -1,5 +1,5 @@
 import java.util.Stack;
-
+/*
 public class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
@@ -38,3 +38,34 @@ public class Solution {
         System.out.println(sol.evalRPN(tokens)); // Output: 9
     }
 }
+*/ //1209 leetcode 
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        // Stack stores character and its frequency
+        Stack<int[]> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek()[0] == ch) {
+                stack.peek()[1]++;
+                if (stack.peek()[1] == k) {
+                    stack.pop(); // remove k duplicates
+                }
+            } else {
+                stack.push(new int[]{ch, 1});
+            }
+        }
+
+        // Build result
+        StringBuilder sb = new StringBuilder();
+        for (int[] pair : stack) {
+            char c = (char) pair[0];
+            int count = pair[1];
+            while (count-- > 0) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+

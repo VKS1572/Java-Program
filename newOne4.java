210321ff07135b0cb391f2cf5357a89e128906b3
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-01-01 22:06:11 +0530

    Implement binary addition in newOne4.java

---
+++ newOne4.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String a = "1010";
+        String b = "1011";
+
+        String result = sol.addBinary(a, b);
+        System.out.println("Result: " + result); // Expected: 10101
+    }
+}
+
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return sb.reverse().toString();
+    }
+}

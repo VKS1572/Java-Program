ea070fe2bdd66c6605fe35fef273d0771b570ee4
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-01-02 21:14:42 +0530

    Create newOne5.java

---
+++ newOne5.java
@@ -0,0 +1,12 @@
+//1️⃣2️⃣ LeetCode 9 – Palindrome Number (Easy)
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int rev = 0, temp = x;
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+        return rev == x;
+    }

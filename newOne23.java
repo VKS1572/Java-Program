7f492f4cf8ee8aeed0062ac909e4724cfce138cc
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-01-22 21:23:46 +0530

    Add problem description for minimum pair removal
    Added problem statement and example for minimum pair removal.

--- newOne23.java
+++ newOne23.java
@@ -1,3 +1,27 @@
+/*
+3507. Minimum Pair Removal to Sort Array I
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array nums, you can perform the following operation any number of times:
+
+Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+Replace the pair with their sum.
+Return the minimum number of operations needed to make the array non-decreasing.
+
+An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+
+Output: 2
+*/
 import java.util.*;
 
 public class Solution {

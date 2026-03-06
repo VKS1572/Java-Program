0ad0288e1484de5b7943f792b5cd65e53a35a300
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-03-06 22:21:00 +0530

    Restore commented-out Array2 methods and classes

--- Array2.java
+++ Array2.java
@@ -190,3 +190,39 @@ class Solution {
     }
 }
 
+/*
+3. Product of Array Except Self
+
+Problem:
+Return an array where each element is product of all other elements except itself.
+
+Example
+
+Input: [1,2,3,4]
+Output: [24,12,8,6]
+
+Java Solution
+
+class ProductArray {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
+
+Time Complexity: O(n)
+*/

3608650f39aacd5dcda4eabd3f7f568ebd2de917
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-03-09 21:54:36 +0530

    Uncomment and implement various algorithms in Array2

--- Array2.java
+++ Array2.java
@@ -226,3 +226,17 @@ class ProductArray {
 
 Time Complexity: O(n)
 */
+// 217 leetcode contain duplicate 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet <Integer> set = new HashSet<>();
+
+        for(int num : nums){
+          if(set.contains(num)){
+            return true;
+          }  
+          set.add(num);
+        }
+        return false;
+    }
+}

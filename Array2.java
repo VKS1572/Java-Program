eefcb159debd5ec6b07812db35d5cfbedd4514b1
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-03-11 22:22:11 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -263,5 +263,26 @@ class Solution {
         return true;
     }
 }*/
+/*
+// grou[p of  anagaram
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
+
+*/
 
 

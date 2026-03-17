983a13b754629c76105c9a3819f02150fe7311e3
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-03-17 22:28:59 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -195,5 +195,18 @@ class Solution {
 }
 
 ```
+//Valid Palindrome............
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // normalize string
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 

9023db0699b3feeb619b72ab2248db1d1cce69fc
Vikas Pradhan <98597145+VKS1572@users.noreply.github.com>
2026-03-20 22:43:34 +0530

    Update Recursion2.java

--- Recursion2.java
+++ Recursion2.java
@@ -109,3 +109,26 @@ public class Recursion2 {
     }
 
 }
+//
+
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;      // handle Integer.MIN_VALUE
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1.0;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {   // if power is odd
+                result *= x;
+            }
+            x *= x;
+            power >>= 1;  // power = power / 2
+        }
+
+        return result;
+    }
+}
